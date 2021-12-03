3448f85565bdc4e8fd046022cbf5732afffba802
am <drizla@inbox.ru>
2021-12-03 11:48:11 +0300

    Fr pre

--- millm/views.py
+++ millm/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from photologue.models import Photo
 
 def vote(request, photo_id):
     return HttpResponse(".%s." % photo_id)
 def index(request):
-    #breakpoint()
     photo_list = Photo.objects.order_by('-date_added')[:5]
-    output = ', '.join([p.image_filename() for p in photo_list])
-    return HttpResponse(output)
+    context = {
+        'photo_list': [(p.id, p.image_filename()) for p in  photo_list],
+    }
+    #breakpoint()
+    return render(request, 'millm/index.html',  context)
